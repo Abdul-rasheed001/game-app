f2d817e361246796b5e617eb6b767cf7ea3d10b5
Abdul-rasheed001 <oadeleke132@gmail.com>
2023-12-13 03:48:28 -0800

    gaming

---
+++ game2.py
@@ -0,0 +1,22 @@
+import mygame
+print("="*50)
+print("&"*15,"WELCOME TO GAMEAPP","&"*15)
+print("="*50)
+print("Choose from the available game")
+while True:
+    gamelist="""
+                1. Guess Number game
+                2. Rock-paper-scissors
+                3. Quit
+            """
+    print(gamelist)
+    print("")
+    choice = int(input("Enter your choice from 1 - 3: "))
+    if choice == 1:
+        mygame.guessnumber()
+    elif choice == 2:
+        mygame.RPS()
+    elif choice == 3:
+        quit()
+    else:
+        pass
